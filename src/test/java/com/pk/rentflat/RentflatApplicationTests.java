e9d7acdf3c1ea7edf31a9618219342d6422c67f5
Roman Vertinsky <raman.viartsinski@azati.com>
2023-04-17 12:19:44 +0200

    create initial github project

---
+++ src/test/java/com/pk/rentflat/RentflatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pk.rentflat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentflatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
