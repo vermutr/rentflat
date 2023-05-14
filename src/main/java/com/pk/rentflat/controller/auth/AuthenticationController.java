9b7e489f3ef8fed5916e99a74144db394f8730c5
Roman Vertinsky <raman.viartsinski@azati.com>
2023-05-14 21:47:12 +0200

    add cors

--- src/main/java/com/pk/rentflat/controller/auth/AuthenticationController.java
+++ src/main/java/com/pk/rentflat/controller/auth/AuthenticationController.java
@@ -5,6 +5,7 @@ import com.pk.rentflat.controller.dto.auth.AuthenticationResponse;
 import com.pk.rentflat.controller.dto.auth.RegisterRequest;
 import com.pk.rentflat.service.auth.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
